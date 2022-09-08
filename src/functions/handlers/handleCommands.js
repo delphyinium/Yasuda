ce77cd07166dee3c98ba9413d8142e2bfad8efc3
delphyinium <tiger.roar@live.com>
2022-09-08 00:22:33 -0400

    Fixed Command Handler

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,20 +1,35 @@
-const fs = require('fs')
-
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
+const fs = require('fs');
+ 
 module.exports = (client) => {
-  client.handleCommands = async () => {
-    const commandFolders = fs.readdirSync("./src/commands");
-    for (const folder of commandFolders) {
-      const commandFiles = fs
-        .readdirSync(`./src/commands/${folder}`)
-        .filter((file) => file.endsWith(".js"));
-
-      const { commands, commandArray } = client;
-      for (const file of commandFiles) {
-        const command = require(`../../commands/${folder}/${file}`);
-        commands.set(command.data.name, command);
-        commandArray.push(command, command.data.toJSON());
-        console.log(`Loaded command ${command.data.name}`);
-      }
-    }
-  };
+    client.handleCommands = async () => {
+        const commandFolder = fs.readdirSync('./src/commands');
+        for (const folder of commandFolder) {
+            const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+ 
+          const {commands, commandArray} = client;
+          for (const file of commandFiles) {
+            const command = require(`../../commands/${folder}/${file}`);
+            commands.set(command.data.name, command);
+            commandArray.push(command.data.toJSON());
+            console.log(`Command: ${command.data.name} has passed through the handler.`);
+          }
+        }
+ 
+        const clientId = '1017263207825543208';
+        const guildId = '1016817040037789808';
+        const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+        try {
+          console.log("Started refreshing application (/) commands.");
+ 
+          await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+            body: client.commandArray,
+          });
+ 
+          console.log("Successfully reloaded application (/) commands.");
+        } catch (error) {
+          console.error(error);
+        }
+    };
 };
