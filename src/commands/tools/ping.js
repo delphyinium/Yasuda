1995ba8bf83e8608adcbd96501e9e972f433fbcb
delphyinium <tiger.roar@live.com>
2022-09-08 00:22:09 -0400

    Added Ping Command

---
+++ src/commands/tools/ping.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction, client) {
+    const message = await interaction.deferReply({
+        fetchReply: true,
+    });
+
+    const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}ms`;
+    await interaction.editReply(newMessage);
+    },
+};
